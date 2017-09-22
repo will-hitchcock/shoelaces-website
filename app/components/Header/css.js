7bc6063644d889d820663014ba5ffc7114e7d4d8
William Hitchcock <willh@getambassador.com>
2017-09-22 14:18:03 -0400

    <Header />, tests, snapshots

---
+++ app/components/Header/css.js
@@ -0,0 +1,10 @@
+const shared = {
+  textAlign: 'center'
+}
+const h1 = { ...shared }
+const h2 = { ...shared }
+const h3 = { ...shared }
+const h4 = { ...shared }
+const h5 = { ...shared }
+
+export default { h1, h2, h3, h4, h5 }
