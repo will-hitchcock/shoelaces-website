2342f0133d2fc394cf547c3fba515373bdb8fe91
William Hitchcock <willh@getambassador.com>
2017-09-22 00:41:14 -0400

    Added <Button />, tests, and snapsnots Formatting

---
+++ app/components/Button/css.js
@@ -0,0 +1,14 @@
+export const css = {
+  display: 'inline-block',
+  borderRadius: '3px',
+  padding: '0.5rem 0',
+  margin: '0.5rem 1rem',
+  width: '11rem',
+  background: 'lightgray',
+  color: 'white',
+  border: '2px solid white',
+  textAlign: 'center',
+  textDecoration: 'none'
+}
+
+export default css
